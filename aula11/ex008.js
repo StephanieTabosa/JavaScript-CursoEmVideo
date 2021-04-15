a0ce5b9610bc089bda28525d88ba554b56f5ff58
StephanieTabosa <stephanie.barreto@hotmail.com>
2021-04-15 17:05:46 -0300

    JavaScript Curso em Video Google

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var velocidade = 60
+
+console.log(`A velocidade do seu carro é de ${velocidade}Km/h`)
+
+if(velocidade > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre com o cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'França'
+console.log(`O país é ${pais}`)
+
+if (pais == 'Brasil'){
+    console.log(`Você é brasileiro`)
+}else{
+    console.log(`Você é estrangeiro`)
+}
